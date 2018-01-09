21f10f94c7468d25bb26e7ba3e0c1b9147f1a88b
Lucas Felix <lfelixsampaio@gmail.com>
2018-01-09 16:14:32 +0000

    encapsulamento

---
+++ Encapsulamento/OlaMundo/Conta.cs
@@ -0,0 +1,37 @@
+﻿namespace OlaMundo
+{
+    class Conta
+    {
+        // propriedade
+        public Cliente Titular { get; set; }
+        public double Saldo { get; private set; }
+        public int Numero { get; set; }
+
+        public bool Saque(double valor)
+        {
+            if(valor <= Saldo)
+            {
+                Saldo = Saldo - valor;
+                return true;
+            }
+            
+            return false;
+        }
+
+        public void Deposito(double valor)
+        {
+            saldo = saldo + valor;
+        }
+
+        public bool Transfere(Conta contaDestino, double valor)
+        {
+            if(Saque(valor))
+            {
+                contaDestino.Deposito(valor);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Encapsulamento/OlaMundo/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OlaMundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Ola Mundo");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string nome = "Thiago";
+            int idade = 21;
+            //peso e altura
+            float altura = 1.8f;
+            double peso = 80.5;
+            decimal preco = 89.9m;
+            char sexo = 'M';
+
+            //true e false
+            bool ehFumante = false;
+
+            // concatenação
+            MessageBox.Show("O cliente é " + nome);
+
+            // 17
+            // 18.9
+
+            double numeroDecimal = 10.9;
+            // casting
+            int numero = (int) numeroDecimal;
+
+            MessageBox.Show("Numero: " + numero);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int idade = 19;
+
+            if(idade > 18)
+            {
+                MessageBox.Show("Você pode entrar na balada");
+            }
+            else
+            {
+                MessageBox.Show("Você nao pode");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // repetição; laços
+            // while, for
+
+            for(int numero = 1; numero <= 100; numero++)
+            {
+                MessageBox.Show("numero: " + numero);
+            }
+
+            int idade = 14;
+
+            // while
+
+            while(idade < 18)
+            {
+                MessageBox.Show("Nao pode");
+                idade++;
+            }
+
+            for( ; idade < 18; idade++)
+            {
+                MessageBox.Show("Nao pode");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int total = 0;
+            for(int i = 1; i <= 1000; i++)
+            {
+                total = i + total;
+            }
+            MessageBox.Show("Soma de 1 a 1000: " + total);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            for(int n = 1;  n <= 100; n++)
+            {
+                if(n % 3 == 0 || n % 4 == 0)
+                {
+                    MessageBox.Show("Multiplo: " + n);
+                }
+
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int fatorial = 1;
+            for(int i = 1; i <= 10; i++)
+            {
+                fatorial = fatorial * i; 
+                MessageBox.Show("Fatorial: " + fatorial);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            /* Conta contaJoao = new Conta();
+            contaJoao.titular = "Joao";
+            contaJoao.saldo = 1000;
+            contaJoao.numero = 123;
+            contaJoao.Saque(400);
+            contaJoao.Deposito(100);
+            
+            Conta contaAna = new Conta();
+            contaAna.titular = "Ana";
+            contaAna.saldo = 2000;
+            contaAna.numero = 234;
+
+            bool sacou = contaAna.Saque(500);
+
+            if(sacou)
+            {
+                MessageBox.Show("Deu certo!");
+            }
+            else
+            {
+                MessageBox.Show("Nao deu certo!");
+            }
+
+            MessageBox.Show("Titular: " + contaAna.titular);
+
+            double valor = 400;
+            contaJoao.Saque(valor);
+            contaAna.Deposito(valor);
+
+            contaJoao.Transfere(contaAna, 10); */
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Cliente tonyStark = new Cliente();
+            tonyStark.nome = "Tony Stark";
+            tonyStark.cpf = "790.432.904-90";
+
+            Conta contaHomemDeFerro = new Conta();
+            contaHomemDeFerro.Deposito(300);
+            // contaHomemDeFerro.titular = tonyStark;
+            contaHomemDeFerro.CriaTitular("Tony");
+            contaHomemDeFerro.Numero = 123;
+
+            contaHomemDeFerro.Saque(500);
+
+            MessageBox.Show("Numero: " + contaHomemDeFerro.Numero);
+            MessageBox.Show("Saldo: " + contaHomemDeFerro.Saldo);
+        }
+    }
+}
