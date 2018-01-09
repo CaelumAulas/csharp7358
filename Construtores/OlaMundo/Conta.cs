ad7a4bb893efc0a9066a9406c696309a472ea11c
Lucas Felix <lfelixsampaio@gmail.com>
2018-01-09 18:22:59 +0000

    construtores

---
+++ Construtores/OlaMundo/Conta.cs
@@ -0,0 +1,48 @@
+﻿namespace OlaMundo
+{
+    class Conta
+    {
+        // propriedade
+        public Cliente Titular { get; set; }
+        public double Saldo { get; private set; }
+        public int Numero { get; set; }
+     
+        public Conta()
+        {
+
+        }
+        
+        public Conta(int numero, double saldoInicial)
+        {
+            this.Saldo = saldoInicial;
+            this.Numero = numero;
+        }
+
+        public bool Saque(double valor)
+        {
+            if(valor <= Saldo)
+            {
+                Saldo = Saldo - valor;
+                return true;
+            }
+            
+            return false;
+        }
+
+        public void Deposito(double valor)
+        {
+            Saldo = Saldo + valor;
+        }
+
+        public bool Transfere(Conta contaDestino, double valor)
+        {
+            if(Saque(valor))
+            {
+                contaDestino.Deposito(valor);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
